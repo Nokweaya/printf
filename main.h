442a2eac61c155c33153468e04c6fcbfb7fb1f2c
Nokweaya <nokweaya@gmail.com>
2023-11-14 00:21:31 -0800

    printf.c

--- main.h
+++ main.h
@@ -5,5 +5,10 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 char *str = va_arg(args, char *);
-
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_percentage(void)
+	
 #endif
---
+++ print_str.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * strlen - returns the length of a string
+ * @str: string pointer
+ * Return: 1
+ */
+int _strlen(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != 0; n++)
+		;
+	return (n);
+}
+/**
+ * _strlenc - strlen function but applied for constant char pointer str
+ * @str: char pointer
+ * Return: n
+ */
+int _strlenc(const char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != 0; n++)
+		;
+	return (n);
+}
